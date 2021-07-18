4d42c33cdbf41438edc5268f3614debc43e66650
cryscan <cryscan@umich.edu>
2021-07-18 11:06:00 -0400

    Add hint.

---
+++ src/game/hint.rs
@@ -0,0 +1,15 @@
+use bevy::prelude::*;
+
+pub struct Hint(pub Entity);
+
+pub fn hint_system(
+    query: Query<(&Transform, &Hint)>,
+    mut hint_query: Query<&mut Transform, Without<Hint>>,
+) {
+    for (transform, hint) in query.iter() {
+        let x = transform.translation.x;
+        if let Ok(mut transform) = hint_query.get_mut(hint.0) {
+            transform.translation.x = x;
+        }
+    }
+}
